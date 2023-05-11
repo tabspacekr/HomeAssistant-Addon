310a4e7e81083172ed8b576f345bb95f3f66c1f7
Terry Kong <82074684+tirasol@users.noreply.github.com>
2023-05-11 20:50:59 +0900

    Update run.sh

--- commax-mqtt2elfin/run.sh
+++ commax-mqtt2elfin/run.sh
@@ -5,6 +5,6 @@ PY_FILE="commax.py"
 DEV_FILE="commax_devinfo.json"
 
 # start server
-echo "[Info] Start commax_mqtt2elfin_python.."
+echo "[Info] Start SEIK Controller.."
 
 python -u $PY_DIR/$PY_FILE
